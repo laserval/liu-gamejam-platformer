fb339d4cfa6318f9549f4a997f04dffb8140f30e
Magnus <thunderviking@gmail.com>
2012-01-28 14:40:15 +0100

    fixed damaged code

--- hydra/src/main/java/hydra/snake/SnakeTileSnakeHead.java
+++ hydra/src/main/java/hydra/snake/SnakeTileSnakeHead.java
@@ -8,25 +8,25 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 
 public class SnakeTileSnakeHead extends SnakeTileSnake {
-	private int headDirection;
-	Image[] headImages = new Image[1];
+	private int headDirection = 0;
+	Image[] headImages = new Image[4];
 	Animation headAnim = new Animation(false);
 
 	public SnakeTileSnakeHead(int x, int y, Rectangle rect, SnakeTileSnake successor) {
 		super(x, y, rect, successor);
-		System.out.print(headDirection);
 
 		// Load background for Snake
 		try {
-			
-			}
-			
+			headImages[0] = new Image("head_tile_right.jpg");
+			headImages[1] = new Image("head_tile_left.jpg");
+			headImages[2] = new Image("head_tile_up.jpg");
+			headImages[3] = new Image("head_tile_down.jpg");
+
 		} catch(SlickException e) {
 			System.out.println(e);
 			return;
 		}
-
-		headAnim.addFrame(headImages[0], 1);
+		headAnim.addFrame(headImages[headDirection], 1);
 	}
 
 	public void render(Graphics g) {
@@ -35,8 +35,13 @@ public class SnakeTileSnakeHead extends SnakeTileSnake {
 
 	public void move(int direction) {
 		moveRecursive();
-		
+
+		/*
+		headAnim.removeFrame(headImages[headDirection], 1);		
+		 
 		headDirection = direction;
+		headAnim.addFrame(headImages[headDirection], 1);
+		*/
 		switch (direction) {
 		default:
 		case SnakeGame.DIRECTION_RIGHT:
@@ -54,8 +59,7 @@ public class SnakeTileSnakeHead extends SnakeTileSnake {
 		case SnakeGame.DIRECTION_DOWN:
 			SnakeGame.instance_.moveHead(x_, y_ + 1);
 			break;
-
-		}
+		}		
 	}
 }
 
