d0efcca5d0f82255d28d66938c0b57d93802492b
Magnus <thunderviking@gmail.com>
2012-01-28 20:28:20 +0100

    Added some corners

---
+++ hydra/src/main/java/hydra/snake/SnakeTileWallRightTop.java
@@ -0,0 +1,39 @@
+package hydra.snake;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+
+public class SnakeTileWallRightTop extends SnakeTile {
+	Image[] wallImages = new Image[1];
+	Animation wallAnim = new Animation(false);
+
+	public SnakeTileWallRightTop(int x, int y, Rectangle rect) {
+		super(x, y, rect);
+
+		try {
+			wallImages[0] = new Image("border_tile_up_right.jpg");
+		} catch(SlickException e) {
+			System.out.println(e);
+			return;
+		}
+
+		wallAnim.addFrame(wallImages[0], 1);
+	}
+
+	public void render(Graphics g) {
+		wallAnim.draw(clipRect_.getX(), clipRect_.getY());
+	}
+
+	public int[] onHeadMovement() {
+		SnakeGame.instance_.onCrash();
+		return new int[] {x_, y_};
+	}
+
+	public String toString() {
+		return "wall";
+	}
+}
